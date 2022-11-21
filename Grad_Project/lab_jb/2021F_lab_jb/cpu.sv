6f4c4fa7ae2ccee977c73b218b43c3ce58a17a3a
Lilian Lamb <57109743+Mysterium-sch@users.noreply.github.com>
2022-11-21 10:21:06 -0500

    adding for second instruction

--- Grad_Project/lab_jb/2021F_lab_jb/cpu.sv
+++ Grad_Project/lab_jb/2021F_lab_jb/cpu.sv
@@ -1,46 +1,61 @@
 module cpu(input logic clk, input logic rst_n, input logic [31:0] instr_in, output logic [31:0] instr_out); 
 
 // From given cpu file
-logic [31:0] inst_ram [4095:0];
-logic [11:0] PC_FETCH = 12'd0; 
-logic [31:0] instr_EX;
-logic [31:0] instr_WB;
+logic [31:0] inst_ram [2047:0];
+logic [11:0] PC_FETCH = 9'd0; 
+logic [63:0] instr_EX;
+logic [31:0] instr_EX_1;
+logic [31:0] instr_EX_2;
 
 // Write back control signals
-logic [3:0] aluop_WB;
-logic [0:0] alusrc_WB;
-logic [1:0] regsel_WB;
-logic [0:0] regwrite_WB;
-logic [0:0] gpio_we_WB;
+logic [1:0] regsel_WB_1;
+logic [0:0] regwrite_WB_1;
+logic [1:0] regsel_WB_2;
+logic [0:0] regwrite_WB_2;
+logic [0:0] gpio_we_WB_2;
 
 // Decode/Execute control signals
-logic [3:0] aluop_EX;
-logic [0:0] alusrc_EX;
-logic [1:0] regsel_EX;
-logic [0:0] regwrite_EX;
-logic [0:0] gpio_we_EX;
+logic [3:0] aluop_EX_1;
+logic [0:0] alusrc_EX_1;
+logic [1:0] regsel_EX_1;
+logic [0:0] regwrite_EX_1;
+logic [0:0] gpio_we_EX_1;
+logic [3:0] aluop_EX_2;
+logic [0:0] alusrc_EX_2;
+logic [1:0] regsel_EX_2;
+logic [0:0] regwrite_EX_2;
+logic [0:0] gpio_we_EX_2;
 
 // regfile outputs
-logic [31:0] readdata1;
-logic [31:0] readdata2;
-logic [31:0] luiHelper;
+logic [31:0] readdata1_1;
+logic [31:0] readdata2_1;
+logic [31:0] luiHelper_1;
+logic [31:0] readdata1_2;
+logic [31:0] readdata2_2;
+logic [31:0] luiHelper_2;
 
 // alu ouputs
-logic [31:0] R_EX;
-logic [31:0] R_WB;
+logic [31:0] R_EX_1;
+logic [31:0] R_WB_1;
+logic [31:0] R_EX_2;
+logic [31:0] R_WB_2;
 logic zero;
 
 // Devices
-logic [31:0] se;
-logic [31:0] mux1;
-logic [31:0] mux2;
-logic [11:0] PC_mux;
+logic [31:0] se_1;
+logic [31:0] mux1_1;
+logic [31:0] mux2_1;
+logic [11:0] PC_mux_1;
+logic [31:0] se_1;
+logic [31:0] mux1_1;
+logic [31:0] mux2_2;
+logic [11:0] PC_mux_2;
 
 // Update to lab 4
-logic [11:0] PC_EX = 12'd0;
-logic [0:0] stall_EX;
-logic [0:0] stall_FETCH;
-logic [1:0] pcsrc_EX;
+logic [11:0] PC_EX_1 = 12'd0;
+logic [0:0] stall_EX_1;
+logic [0:0] stall_FETCH_1;
+logic [1:0] pcsrc_EX_1;
 
 //branch
 logic [11:0] branch_addr_EX;
@@ -70,45 +85,51 @@ always_ff @(posedge clk) begin
 	end 
 end 
 
+assign instr_EX_1 = instr_EX[63:32];
+assign instr_EX_2 = instr_EX[31:0];
 
 // Execute and Decode stage
 
 // intialize control signals to be decode/execute
-control control1 (.instr(instr_EX), .stall_EX(stall_EX), .aluop(aluop_EX), .alusrc(alusrc_EX), .regsel(regsel_EX), .regwrite(regwrite_EX), .gpio_we(gpio_we_EX)); 
+control control1 (.instr(instr_EX_1), .stall_EX(stall_EX_1), .aluop(aluop_EX_1), .alusrc(alusrc_EX_1), .regsel(regsel_EX_1), .regwrite(regwrite_EX_1), .gpio_we(gpio_we_EX_1)); 
+control control2 (.instr(instr_EX_2), .stall_EX(stall_EX_1), .aluop(aluop_EX_2), .alusrc(alusrc_EX_2), .regsel(regsel_EX_2), .regwrite(regwrite_EX_2), .gpio_we(gpio_we_EX_2)); 
 
 // Sign extend for alu
-assign se = {{20{instr_EX[31]}}, instr_EX[31:20]};
-assign mux1 = (alusrc_EX == 2'b1) ? se : readdata2;
+assign se_1 = {{20{instr_EX_1[31]}}, instr_EX_1[31:20]};
+assign mux1_1 = (alusrc_EX_1 == 2'b1) ? se_1 : readdata2_1;
+
+assign se_2 = {{20{instr_EX_2[31]}}, instr_EX_2[31:20]};
+assign mux1_2 = (alusrc_EX_2 == 2'b1) ? se_2 : readdata2_2;
 
 
 
-logic [6:0] op;
-assign op = instr_EX[6:0];
-alu alu1 (.A(readdata1), .B(mux1), .op(aluop_EX), .R(R_EX), .zero(zero));
+alu alu1_1 (.A(readdata1_1), .B(mux1_1), .op(aluop_EX_1), .R(R_EX_1), .zero(zero));
+alu alu1_2 (.A(readdata1_2), .B(mux1_2), .op(aluop_EX_2), .R(R_EX_2), .zero(zero));
 
 // intialize alu
-	assign pcsrc_EX = (stall_EX == 1'b1) ? 2'b0 : (instr_EX[6:0] == 7'b1100011) ? 
-					((instr_EX[14:12] == 3'b0) ? (R_EX == 32'b0 ? 2'b1 : 2'b0) : ((instr_EX[14:12] == 3'b1) ? (R_EX !== 32'b0 ? 2'b1 : 2'b0) : (instr_EX[14:12] == 3'b100) ? (R_EX == 32'b1 ? 2'b1 : 2'b0) : (instr_EX[14:12] == 3'b101) ? (R_EX == 32'b0 ? 2'b1 : 2'b0) : (instr_EX[14:12] == 3'b110) ? (R_EX == 32'b1 ? 2'b1 : 2'b0) : (instr_EX[14:12] == 3'b111) ? (R_EX == 32'b0 ? 2'b1 : 2'b0) : 2'b0))
-								: ((instr_EX[6:0] == 7'b1100111) ? 2'b11 : ((instr_EX[6:0] == 7'b1101111) ? 2'b10 : 2'b0));
+	assign pcsrc_EX_1 = (stall_EX_1 == 1'b1) ? 2'b0 : (instr_EX_1[6:0] == 7'b1100011) ? 
+			    ((instr_EX_1[14:12] == 3'b0) ? (R_EX_1 == 32'b0 ? 2'b1 : 2'b0) : ((instr_EX_1[14:12] == 3'b1) ? (R_EX_1 !== 32'b0 ? 2'b1 : 2'b0) : (instr_EX_1[14:12] == 3'b100) ? (R_EX_1 == 32'b1 ? 2'b1 : 2'b0) : (instr_EX_1[14:12] == 3'b101) ? (R_EX_1 == 32'b0 ? 2'b1 : 2'b0) : (instr_EX_1[14:12] == 3'b110) ? (R_EX_1 == 32'b1 ? 2'b1 : 2'b0) : (instr_EX_1[14:12] == 3'b111) ? (R_EX_1 == 32'b0 ? 2'b1 : 2'b0) : 2'b0))
+			    : ((instr_EX_1[6:0] == 7'b1100111) ? 2'b11 : ((instr_EX_1[6:0] == 7'b1101111) ? 2'b10 : 2'b0));
 	
-	assign stall_FETCH = (pcsrc_EX[1:0] == 2'b0) ? 1'b0 : 1'b1;
+	assign stall_FETCH_1 = (pcsrc_EX_1[1:0] == 2'b0) ? 1'b0 : 1'b1;
 
-always @(posedge clk) stall_EX <= stall_FETCH;
+	always @(posedge clk) stall_EX_1 <= stall_FETCH_1;
 
 // Write back stage
 always_ff @(posedge clk) begin
 
-	R_WB <= R_EX;
-	instr_WB <= instr_EX;
-	regwrite_WB <= regwrite_EX;
-	regsel_WB <= regsel_EX;
-	gpio_we_WB <= gpio_we_EX;
-	alusrc_WB <= alusrc_EX;
-	aluop_WB <= aluop_EX;
-	luiHelper <= ({instr_EX[31:12], 12'b0});
+	R_WB_1 <= R_EX_1;
+	R_WB_2 <= R_EX_2;
+	regwrite_WB_1 <= regwrite_EX_1;
+	regwrite_WB_2 <= regwrite_EX_1;
+	regsel_WB_1 <= regsel_EX_1;
+	regsel_WB_2 <= regsel_EX_2;
+	luiHelper_1 <= ({instr_EX_1[31:12], 12'b0});
+	luiHelper_2 <= ({instr_EX_2[31:12], 12'b0});
+	gpio_we_WB_2 <= gpio_we_EX_2;
 	
 // Setting our output
-	if(gpio_we_WB == 1'b1) begin
+	if(gpio_we_WB_2 == 1'b1) begin
 		instr_out <= readdata1;
 	end else if (~rst_n) begin 
 	        instr_out <= 32'b0;           
@@ -118,28 +139,29 @@ always_ff @(posedge clk) begin
 end
 
 // Mux for regfile
-assign mux2 = (regsel_WB == 2'b11) ? PC_EX : ((regsel_WB == 2'b10) ? R_WB : ((regsel_WB == 2'b01) ? luiHelper : instr_in));
+assign mux2_1 = (regsel_WB_1 == 2'b11) ? PC_EX : ((regsel_WB_1 == 2'b10) ? R_WB_1 : ((regsel_WB_1 == 2'b01) ? luiHelper_1 : instr_in));
+assign mux2_2 = (regsel_WB_2 == 2'b11) ? PC_EX : ((regsel_WB_2 == 2'b10) ? R_WB_2 : ((regsel_WB_2 == 2'b01) ? luiHelper_2 : instr_in));
 
 // Lab 4 Specfics 
 
 // branch cal
-assign branch_offset_EX = {instr_EX[31], instr_EX[7], instr_EX[30:25], instr_EX[11:8], 1'b0};
+assign branch_offset_EX = {instr_EX_1[31], instr_EX_1[7], instr_EX_1[30:25], instr_EX_1[11:8], 1'b0};
 assign branch_addr_EX = PC_EX + {branch_offset_EX[12], branch_offset_EX[12:2]};
 
 // Jal cal
-assign jal_offset_EX = {instr_EX[31], instr_EX[19:12], instr_EX[20], instr_EX[30:21], 1'b0};
+assign jal_offset_EX = {instr_EX_1[31], instr_EX_1[19:12], instr_EX_1[20], instr_EX_1[30:21], 1'b0};
 assign jal_addr_EX = PC_EX + jal_offset_EX[13:2];
 
 //Jalr cal
-assign jalr_offset_EX = instr_EX[31:20];
-assign jalr_addr_EX = readdata1 + {{2{jalr_offset_EX[11]}},jalr_offset_EX[11:2]};
+assign jalr_offset_EX = instr_EX_1[31:20];
+assign jalr_addr_EX = readdata1_1 + {{2{jalr_offset_EX[11]}},jalr_offset_EX[11:2]};
 
 logic [11:0] holder;
-assign holder = PC_FETCH + 1'b1;
+assign holder = PC_FETCH + 2'b10;
 assign PC_mux = (pcsrc_EX == 2'b11) ? (jalr_addr_EX) : ((pcsrc_EX == 2'b1) ? branch_addr_EX : ((pcsrc_EX == 2'b10) ? jal_addr_EX : holder));
 		
 // Intialize regfile
-regfile regfile1 (.clk(clk), .rst(~rst_n), .we(regwrite_WB), .readaddr1(instr_EX[19:15]), .readaddr2(instr_EX[24:20]), .writeaddr(instr_WB[11:7]), .writedata(mux2), .readdata1(readdata1), .readdata2(readdata2));
+	regfile regfile1 (.clk(clk), .rst(~rst_n), .we_1(regwrite_WB_1), .we_2(regwrite_WB_2), .readaddr1_1(instr_EX_1[19:15]), .readaddr1_1(instr_EX_2[19:15]), .readaddr2_2(instr_EX_2[24:20]), .readaddr2_2(instr_EX_2[24:20]), .writeaddr_1(instr_WB_1[11:7]), .writeaddr_2(instr_WB_2[11:7]), .writedata_1(mux2_1), .writedata_2(mux2_2), .readdata1_1(readdata1_1), .readdata2_1(readdata2_1), .readdata1_2(readdata1_2), .readdata2_2(readdata2_2));
 
 
 endmodule  
